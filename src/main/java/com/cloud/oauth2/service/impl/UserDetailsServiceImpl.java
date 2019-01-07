a1509aa45a8b5f74786a3e4799083322e927e1b1
hzwy23 <hzwy23@hzwy23s-MacBook-Pro.local>
2019-01-07 23:39:13 +0800

    init

--- src/main/java/com/cloud/oauth2/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/com/cloud/oauth2/service/impl/UserDetailsServiceImpl.java
@@ -1,4 +1,4 @@
-package com.cloud.com.cloud.oauth2.service.impl;
+package com.cloud.oauth2.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.AuthorityUtils;
